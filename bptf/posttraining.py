f58cbb1af1ad78ff10873babdb6292143731429b
iceboal <iceboal@gmail.com>
2014-05-22 16:37:30 -0400

    Update psottraining.py

--- bptf/posttraining.py
+++ bptf/posttraining.py
@@ -6,8 +6,8 @@ Out-of-vocabulary embedding
 """
 rootPath = '../data/'
 pmfPath = rootPath + 'bptf/roget_mm'
-matVocPath = rootPath + "bpth/roget_mm-voc"
+matVocPath = rootPath + "bptf/roget_mm-voc"
 grePath = rootPath + "test-data/gre_wordlist.txt"
-savePath = rootPath + "test-data/gre_oov.txt"
+savePath = rootPath + "bptf/gre_oov.txt"
 
-oov.embedding(pmfPath, matVocPath, grePath, savePath)
+oov.embedding(pmfPath, matVocPath, grePath, savePath, 58)
